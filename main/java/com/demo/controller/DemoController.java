26edcef2df793d26e19564b3af149b41203ae114
test <test@test.com>
2019-10-12 17:25:08 +0800

    增加democontroller

---
+++ main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,11 @@
+package com.demo.controller;
+
+/**
+ * description:
+ * Created by Ray on 2019-10-12
+ */
+public class DemoController {
+    public void test1(){
+        System.out.println("hello tom...");
+    }
+}
